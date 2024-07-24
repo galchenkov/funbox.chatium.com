a12a56f9469b4ef1133213fe7cf103e7016cefb0
Aleksandr Galchenkov <galchenkov@yandex.ru>
2024-07-24 14:11:36 +0300

    Galchenkov feature

--- plugin/index.tsx
+++ plugin/index.tsx
@@ -1,15 +1,15 @@
-import { jsx } from '@app/html-jsx'
+import { jsx } from "@app/html-jsx";
 
-app.html('/', async (ctx) => {
+app.html("/", async (ctx) => {
   return (
     <html>
       <head>
         <title>Fun Box</title>
       </head>
       <body>
-        <h1>Main branch</h1>
+        <h1>Galchenkov Release Main branch</h1>
         <p>{Date.now()}</p>
       </body>
     </html>
-  )
-})
+  );
+});
